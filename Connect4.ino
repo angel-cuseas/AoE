6a991ffb60b3b5893991258a419b0ec8039b4d47
Nathan Coulibaly <31783135+NateCoul23@users.noreply.github.com>
2018-11-30 03:48:49 -0500

    Add files via upload

--- Connect4.ino
+++ Connect4.ino
@@ -8,7 +8,7 @@
 #define C   A2
 
 int columns[] = {14,14,14,14,14,14,14,14,14,14};
-int selections [][10] = { {0,0,0,0,0,0,0,0,0,0},
+ int selections [][10] = { {0,0,0,0,0,0,0,0,0,0},
                         {0,0,0,0,0,0,0,0,0,0},
                         {0,0,0,0,0,0,0,0,0,0},
                         {0,0,0,0,0,0,0,0,0,0},
@@ -23,6 +23,7 @@ int selections [][10] = { {0,0,0,0,0,0,0,0,0,0},
 RGBmatrixPanel matrix (A, B, C, CLK, LAT, OE, false);
 
 void setup() {
+  Serial.begin(9600);
   pinMode(11,INPUT);
   matrix.begin();
   matrix.setTextColor(matrix.Color333(7,0,0));
@@ -42,76 +43,197 @@ void setup() {
   matrix.print("Four!!!");
   delay(1000);
   matrix.fillScreen(0);
-  matrix.drawRect(4,0,2,16, matrix.Color333(0,0,7));
-  matrix.drawRect(26,0,2,16, matrix.Color333(0,0,7));
 }
 
 void loop() {
-  int win=0;
-  while(win==0){
+  matrix.drawRect(4,2,2,16, matrix.Color333(0,0,7));
+  matrix.drawRect(26,2,2,16, matrix.Color333(0,0,7));
+  int win = 0;
+  while(win==0){    
     matrix.setCursor(0,0);
     matrix.setTextColor(matrix.Color333(255,255,0));
     matrix.print("1");
+    matrix.drawRect(4,2,2,16, matrix.Color333(0,0,7));
+    matrix.fillRect(4,0,2,2, matrix.Color333(0,0,0));
     int xPos = 6;
-    while(digitalRead(11)!=HIGH){
-      if(analogRead(A3)>512){
-        delay(800);
+    int p1selection = 0;
+    matrix.drawRect(6,0,2,2, matrix.Color333(255,255,0));
+    while(p1selection==0){
+      if(digitalRead(11)==HIGH){
+        p1selection++;
+        break;
+      }
+      delay(500);
+      if(analogRead(A3)>530){
         if(xPos<24){
-          delay(1000);
           matrix.fillRect(xPos-2,0,2,2, matrix.Color333(0,0,0));
-          if(xPos==6){
-            matrix.drawRect(xPos-2,0,2,2, matrix.Color333(0,0,7));
-          }
           xPos = xPos+2;
           matrix.drawRect(xPos,0,2,2, matrix.Color333(255,255,0));
+          matrix.fillRect(xPos-2,0,2,2, matrix.Color333(0,0,0));
         }
       }
-      else if(analogRead(A3)<512){
-        delay(800);
-        if(xPos>6){
-        xPos = xPos-2;
-        matrix.fillRect(xPos-2,0,2,2, matrix.Color333(0,0,0));
-        xPos = xPos+2;
-        matrix.drawRect(xPos,0,2,2, matrix.Color333(255,255,0));
-      }
+      else if(analogRead(A3)<500){
+       if(xPos>6){
+          matrix.fillRect(xPos+2,0,2,2, matrix.Color333(0,0,0));
+          xPos = xPos-2;
+          matrix.drawRect(xPos,0,2,2, matrix.Color333(255,255,0));
+          matrix.fillRect(xPos+2,0,2,2, matrix.Color333(0,0,0));
+        }
       }
     }
-    if(digitalRead(11)==HIGH){
-      matrix.fillRect(xPos-2,0,2,2, matrix.Color333(0,0,0));
-      selections[xPos-6][columns[xPos-6]] = 1;
+    matrix.fillRect(xPos,0,2,2, matrix.Color333(0,0,0));
+    selections[(xPos-6)/2][((columns[(xPos-6)/2])-2)/2]=1;//[0][6]
+    matrix.drawRect(xPos,columns[(xPos-6)/2],2,2, matrix.Color333(255,255,0));
+    columns[(xPos-6)/2] = columns[(xPos-6)/2] - 2;
+    if(connectFourPlayer1(selections)){
+      win++;
     }
+    delay(1000);
     matrix.fillRect(0,0,4,7,matrix.Color333(0,0,0));
-    matrix.setCursor(28,0);
+    
+    //player 2
+    if(win==0){
+    matrix.setCursor(0,0);
     matrix.setTextColor(matrix.Color333(255,0,0));
     matrix.print("2");
     xPos = 6;
-    while(digitalRead(11)!=HIGH){
-      if(analogRead(A3)>512){
-        delay(800);
+    int p2selection = 0;
+    matrix.drawRect(6,0,2,2, matrix.Color333(255,0,0));
+    while(p2selection==0){
+      if(digitalRead(11)==HIGH){
+        p2selection++;
+        break;
+      }
+      delay(500);
+      if(analogRead(A3)>530){
         if(xPos<24){
           matrix.fillRect(xPos-2,0,2,2, matrix.Color333(0,0,0));
           xPos = xPos+2;
-          matrix.drawRect(xPos,0,2,2, matrix.Color333(255,255,0));
+          matrix.drawRect(xPos,0,2,2, matrix.Color333(255,0,0));
+          matrix.fillRect(xPos-2,0,2,2, matrix.Color333(0,0,0));
         }
       }
-      else if(analogRead(A3)<512){
-        delay(800);
-        if(xPos>6){
-        xPos = xPos-2;
-        matrix.fillRect(xPos-2,0,2,2, matrix.Color333(0,0,0));
-        if(xPos==6){
-            matrix.drawRect(xPos-2,0,2,2, matrix.Color333(0,0,7));
-          }
-        xPos = xPos+2;
-        matrix.drawRect(xPos,0,2,2, matrix.Color333(255,255,0));
-      }
+      else if(analogRead(A3)<500){
+       if(xPos>6){
+          matrix.fillRect(xPos+2,0,2,2, matrix.Color333(0,0,0));
+          xPos = xPos-2;
+          matrix.drawRect(xPos,0,2,2, matrix.Color333(255,0,0));
+          matrix.fillRect(xPos+2,0,2,2, matrix.Color333(0,0,0));
+        }
       }
     }
-    if(digitalRead(11)==HIGH){
-      matrix.fillRect(xPos-2,0,2,2, matrix.Color333(0,0,0));
-      selections[xPos-6][columns[xPos-6]] = 2;
+    matrix.fillRect(xPos,0,2,2, matrix.Color333(0,0,0));
+    selections[(xPos-6)/2][((columns[(xPos-6)/2])-2)/2]=2;
+    matrix.drawRect(xPos,columns[(xPos-6)/2],2,2, matrix.Color333(255,0,0));
+    columns[(xPos-6)/2] = columns[(xPos-6)/2] - 2;
+    delay(1000);
+    matrix.fillRect(0,0,4,7,matrix.Color333(0,0,0));
+    if(connectFourPlayer2(selections) == true){
+      win++;
     }
-    matrix.fillRect(28,0,4,7, matrix.Color333(0,0,0));
   }
+  }
+  if(connectFourPlayer1(selections) == true){
+      matrix.fillRect(0,0,31,15,matrix.Color333(0,0,0));
+      matrix.setCursor(0,0);
+      matrix.print("P1");
+      matrix.setCursor(0,8);
+      matrix.print("WINS");
+      matrix.fillRect(0, 15, 31, 1,matrix.Color333(0,0,0));
+      delay(5000);
+      matrix.fillRect(0,0,31,15,matrix.Color333(0,0,0));
+    }
+   if(connectFourPlayer2(selections) == true){
+      matrix.fillRect(0,0,31,15,matrix.Color333(0,0,0));
+      matrix.setCursor(0,0);
+      matrix.print("P2");
+      matrix.setCursor(0,8);
+      matrix.print("WINS");
+      matrix.fillRect(0, 15, 31, 1,matrix.Color333(0,0,0));
+      delay(5000);
+      matrix.fillRect(0,0,31,15,matrix.Color333(0,0,0));
+    }
+    for(int i=0; i<sizeof(columns); i++){
+      columns[i]=14;
+    }
+    for(int i=0; i<7; i++){
+      for(int j=0; j<10; j++){
+        selections[i][j]=0;
+      }
+    }
+}
+
+boolean connectFourPlayer1(int [][10]){
+  int fourInARow = 0;
+  for(int i=0; i<7; i++){//horizontal
+    for(int j=0; j<10; j++){
+      if(selections[i][j]==1 && selections[i][j+1]==1 && selections[i][j+2]==1 && selections[i][j+3]==1){
+        fourInARow+=1;
+      }
+    }
+  }
+ for(int i=0; i<7; i++){//vertical
+    for(int j=0; j<10; j++){
+      if(selections[i][j]==1 && selections[i+1][j]==1 && selections[i+2][j]==1 && selections[i+3][j]==1){
+        fourInARow+=1;
+      }
+    }
+  }
+  for(int i=0; i<7; i++){//diagonal right
+    for(int j=0; j<10; j++){
+      if(selections[i][j]==1 && selections[i+1][j+1]==1 && selections[i+2][j+2]==1 && selections[i+3][j+3]==1){
+        fourInARow+=1;
+      }
+    }
+  }
+ for(int i=6; i>=0; i--){//diagonal left
+    for(int j=0; j<10; j++){
+      if(selections[i][j]==1 && selections[i-1][j+1]==1 && selections[i-2][j+2]==1 && selections[i-3][j+3]==1){
+        fourInARow+=1;
+      }
+    }
+  }
+    
+  if(fourInARow!=0){
+    return true;
+  }
+  return false;
+}
+
+boolean connectFourPlayer2(int [][10]){
+  int fourInARow = 0;
+  for(int i=0; i<7; i++){//horizontal
+    for(int j=0; j<10; j++){
+        if(selections[i][j]==2 && selections[i][j+1]==2 && selections[i][j+2]==2 && selections[i][j+3]==2){
+          fourInARow+=1;
+      }
+    }
+  }
+ for(int i=0; i<7; i++){//vertical
+    for(int j=0; j<10; j++){
+      if(selections[i][j]==2 && selections[i+1][j]==2 && selections[i+2][j]==2 && selections[i+3][j]==2){
+        fourInARow+=1;
+      }
+    }
+  }
+  for(int i=0; i<7; i++){//diagonal right
+    for(int j=0; j<10; j++){
+      if(selections[i][j]==2 && selections[i+1][j+1]==2 && selections[i+2][j+2]==2 && selections[i+3][j+3]==2){
+        fourInARow+=1;
+      }
+    }
+  }
+ for(int i=6; i>=0; i--){//diagonal left
+    for(int j=0; j<10; j++){
+      if(selections[i][j]==2 && selections[i-1][j+1]==2 && selections[i-2][j+2]==2 && selections[i-3][j+3]==2){
+        fourInARow+=1;
+      }
+    }
+  }
+ 
+  if(fourInARow!=0){
+    return true;
+  }
+  return false;
 }
   
